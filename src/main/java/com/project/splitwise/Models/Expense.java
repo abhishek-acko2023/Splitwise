e1fdf7281b375fa27586576728898569a7d07d71
Anupam Jha <anupam.jha@ACKO2038.local>
2023-02-07 13:19:53 +0530

    expense

--- src/main/java/com/project/splitwise/Models/Expense.java
+++ src/main/java/com/project/splitwise/Models/Expense.java
@@ -1,4 +1,54 @@
 package com.project.splitwise.Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "expenseTable")
 public class Expense {
+    @Id
+    @SequenceGenerator(
+            name = "expense_id_sequence",
+            sequenceName = "expense_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "expense_id_sequence"
+    )
+    @Column(
+            name = "expense_id",
+            nullable = false
+    )
+    private UUID expenseId ;
+    @Column(
+            name="expense_description",
+            nullable = false
+    )
+    private String expenseDescription ;
+
+    @Column(
+            name="expense_amount",
+            nullable = false
+    )
+    private Double amount ;
+
+//    @Column(
+//            name="paid_by",
+//            nullable = false
+//    )
+//    private User paidBy ;
+//
+//    @Column(
+//            name="expense_members",
+//            nullable = false
+//    )
+//    private List<User> paidTo ;
+
 }
